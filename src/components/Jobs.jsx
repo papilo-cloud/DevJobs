da59f8e4999f3e8aaaf2ac451bb2412186e8f3da
Abdulrahman <abdulrahmanbadamasi123@gmail.com>
2022-08-13 09:15:24 +0100

    added an animation on-scrollin-in, using get bounding client

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 import { useContext } from 'react'
 import { GlobalContext } from './context/UserContext'
 import Job from './Job'
 
 
 const Jobs = () => {
-    const {data, visible} = useContext(GlobalContext)
-    // console.log(visible)
-
+    const [more, setMore] = useState(10)
+    const [show, setShow] = useState(true)
+    const {data} = useContext(GlobalContext)
+   
+    
     return (
-    <div className='jobs'>
-        {data.map((job, x) =>  <Job key={x} {...job} visible={visible} />)}
-    </div>
+      <>
+      <div className='jobs'>
+          {data.slice(0,more).map((job, x) =>  <Job key={x} {...job} />)}
+      </div>
+      <button className='btn' style={{display: show? 'block':'none'}} 
+      onClick={() =>{setMore(prev => prev + 5); setShow(!show)}}>More</button>
+    </>
   )
 }
 
