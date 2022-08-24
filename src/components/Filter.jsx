00d6ad61440f4f7e315fbfa4705705850d891fcb
Abdulrahman <abdulrahmanbadamasi123@gmail.com>
2022-08-24 01:19:35 +0100

    added some stylings

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,32 +1,33 @@
 import React from 'react'
 import { useContext } from 'react';
 import second from '../assets/desktop/icon-search.svg'
+import search from '../assets/desktop/icon-locate.svg'
+import location from '../assets/desktop/icon-location.svg'
 import first from '../assets/mobile/icon-filter.svg'
 import { GlobalContext } from './context/UserContext';
+import { useState } from 'react';
 
 const Filter = () => {
+
+    const [show, setShow] = useState(false)
+
     const x = window.innerWidth >= 1000 ? 'Full Time Only':'Full Time';
     const y = window.innerWidth >= 1000 ? 'Filter by title, companies, expertise...':'Filter by title...';
     const {theme} = useContext(GlobalContext)
-    const searchInput = (
-    <>
-      <label className='label1'>
-                    <input type="text"  className='text2' placeholder='Filter by location...'/>
-                </label>
-               <label className='label2'>
-                    <input type="checkbox" className='checkbox' />
-                    <span>{x}</span>
-                    <input type="submit" value="Search" />
-               </label>
-    </>
-    )
+    console.log(show)
+    // const chnadeShow = () =>{
+    //   return setShow(!show)
+    // }
+   
   return (
     <div className='filter'>
             <form className={theme? 'form darks_theme':'form'}>
+              {/* <div> */}
                 <label className='label'>
+                <img src={search} alt="search"/>
                 <input type="text" className='text1' placeholder={y} />
                     <div className='search'>
-                        <button className='btn1' type='button'>
+                        <button className='btn1' type='button' onClick={() => setShow(!show)}>
                           <img src={first} alt="search" />
                         </button>
                         <button className='btn2' type='button'>
@@ -34,7 +35,19 @@ const Filter = () => {
                         </button>
                     </div>
                 </label>
-                {searchInput}
+                <div className={show ? 'lbl show': 'lbl'}>
+        <label className='label1'>
+          <img src={location} alt="location" />
+            <input type="text"  className='text2' placeholder='Filter by location...'/>
+        </label>
+        <div className='label2'>
+            <label>
+              <input type="checkbox" className='checkbox' />
+              <span>{x}</span>
+            </label>
+            <input type="submit" value="Search" />
+        </div>
+    </div>
             </form>
     </div>
   )
